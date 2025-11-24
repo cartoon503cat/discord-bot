6b8cc9a2185d7bfa88b88969111e1245aebc40a8
cartoon503cat <mihail503misha@gmail.com>
2025-11-24 16:41:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -150,6 +150,14 @@ async def on_message(message):
     if bot.user.mentioned_in(message):
         await message.reply("Так мій котику)) 😻")
 
+    if not responded and "смачного" in content:
+        await message.reply("І тобі приємного, моя карамелько🍭😋🍬")
+        responded = True
+
+    if not responded and "приємного" in content:
+        await message.reply("І тобі приємного, моя карамелько🍭😋🍬")
+        responded = True
+
     # ПРАВИЛА
 
     
@@ -386,6 +394,7 @@ if __name__ == "__main__":
         print("⛔ ERROR: TOKEN не знайдено в ENV")
     else:
         bot.run(TOKEN)
+
 
 
 
